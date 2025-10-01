0514375e071e3b8dacee020436d877991345f5eb
emersonandre <emersonsilvestrin@live.com>
2025-10-01 09:34:18 -0300

    feat-usestate

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 export function App() {
   const [count, setCount] = useState(0);
+  const [hide, setHide] = useState(false);
 
   return (
     <>
+      {hide && <p>Texto 1</p>}
+      {!hide && <p>Texto 2</p>}
+      {hide ? <p>Texto 1</p> : <p>Texto 2</p>}
       <button onClick={() => setCount(count + 1)}> contar {count} </button>
+      <button onClick={() => setHide(!hide)}> Toggle </button>
     </>
   );
 }
